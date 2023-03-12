160bc563f14598a551ccfa13ca1ba896f95f35a2
GuillermoA231 <andradajoaquin302@gmail.com>
2023-03-12 14:42:48 -0300

    ejercicio 6 terminado

---
+++ js/ejercicios/06-ejercicio.js
@@ -0,0 +1,29 @@
+/*6- Realiza un script que escriba una pirámide del 1 al 30 de la siguiente forma :
+
+1
+22
+333
+4444
+55555
+666666
+…….
+
+ */
+
+
+const letrasDni = 'TRWAGMYFPDXBNJZSQVHLCKE';
+let letrasIndex = 0;
+let resultado = '';
+let dni = prompt("Introducir número DNI sin puntos y entre entre 0 y 99999999: ")
+let numDni = parseInt(dni);
+console.log(numDni);
+while (numDni === null || isNaN(numDni) || numDni < 0 || numDni > 99999999) {
+    alert("Valor incorrecto! introduce un numero entre 0 y 99999999")
+    dni = prompt("INTODUCE EL NUMERO DNI SIN PUNTOS Y MAYOR A 0 Y MENOR A 99999999");
+    numDni = parseInt(dni);
+}
+letrasIndex = numDni % 23;
+resultado = letrasDni[letrasIndex];
+
+alert(resultado);
+console.log(resultado);
