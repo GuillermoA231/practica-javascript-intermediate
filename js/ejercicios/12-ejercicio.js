56226f39783a76af34f9898b7a0e2819d0c7a12b
GuillermoA231 <andradajoaquin302@gmail.com>
2023-03-12 21:45:49 -0300

    function ejercicio12() creado

--- js/ejercicios/12-ejercicio.js
+++ js/ejercicios/12-ejercicio.js
@@ -1,4 +1,5 @@
-/**12- Realiza un script que genere un número aleatorio entre 1 y 99 */
+function ejercicio12(){
+  /**12- Realiza un script que genere un número aleatorio entre 1 y 99 */
 function obtenerIntAleatorio(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
@@ -8,3 +9,4 @@ let numero;
 
 numero = obtenerIntAleatorio(MIN,MAX);
 console.log(numero);
+}
