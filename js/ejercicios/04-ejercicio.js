bcc5946ca5f2e8a1f01bb6443099765a971209b9
GuillermoA231 <andradajoaquin302@gmail.com>
2023-03-13 19:50:48 -0300

    arregla ejercicio 4

--- js/ejercicios/04-ejercicio.js
+++ js/ejercicios/04-ejercicio.js
@@ -1,24 +1,24 @@
-function ejercicio4(){
+function ejercicio4() {
   /*
 4- Realiza un script que pida números hasta que se pulse “cancelar”. 
 Si no es un número deberá indicarse con un «alert» y seguir pidiendo números. 
 Al salir con “cancelar” deberá indicarse la suma total de los números introducidos.
  */
-let sumaNum = 0;
-let num;
-
-do {
-  num = prompt("Ingrese un numero:");
-  if (num !== null) {
-    if (typeof num === typeof NaN) {
-      alert("No es un número válido. Intente de nuevo.");
-    } else {
-        num = parseInt(num);
-        sumaNum = sumaNum + num;
+  let sumaNum = 0;
+  let num;
+  let booleano = true;
+  while (booleano) {
+    num = prompt("Introduce un número:");
+    switch (true) {
+      case num === null:
+        alert("La suma de los números es: " + sumaNum);
+        booleano = false;
+        break;
+      case isNaN(num):
+        alert("Intente de nuevo, no es un número valido.");
+        break;
+      default:
+        sumaNum += parseInt(num);
     }
   }
-} while (isNaN(num));
-
-alert("La suma de los números es: " + sumaNum);
-
 }
