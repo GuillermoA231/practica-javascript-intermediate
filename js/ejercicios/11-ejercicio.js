2f5b802e3afdf16e26c6d77befea11d54a61982a
GuillermoA231 <andradajoaquin302@gmail.com>
2023-03-12 21:45:30 -0300

    function ejercicio11() creado

--- js/ejercicios/11-ejercicio.js
+++ js/ejercicios/11-ejercicio.js
@@ -1,4 +1,5 @@
-let nombreYApellido = [];
+function ejercicio11(){
+  let nombreYApellido = [];
 for (let i = 0; i < 3; i++) {
   let nombre = prompt(`Ingresa el nombre ${i + 1}:`);
   let edad = parseInt(prompt(`Ingresa la edad de ${nombre}:`));
@@ -8,3 +9,4 @@ let edades = nombreYApellido.map(persona =>persona.edad);
 let edadMasAlta = Math.max(...edades);
 let personaMasVieja = nombreYApellido.find(persona => persona.edad == edadMasAlta);
 console.log(personaMasVieja);
+}
