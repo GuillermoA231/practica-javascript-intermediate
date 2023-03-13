f8660b1daf636ee3cfb8f0dd76793eae4efd65d7
GuillermoA231 <andradajoaquin302@gmail.com>
2023-03-12 21:47:33 -0300

    function ejercicio16() creado

--- js/ejercicios/16-ejercicio.js
+++ js/ejercicios/16-ejercicio.js
@@ -1,4 +1,5 @@
-/*16- Realiza un script que pida una cadena de texto y la devuelva al revés. Es decir, si tecleo “hola que tal” deberá mostrar “lat euq aloh”.
+function ejercicio16(){
+    /*16- Realiza un script que pida una cadena de texto y la devuelva al revés. Es decir, si tecleo “hola que tal” deberá mostrar “lat euq aloh”.
 */
 
 let texto = prompt("introduzca su texto");
@@ -10,3 +11,4 @@ console.log(revertirArray);
 
 const unirArray =revertirArray.join(``);
 console.log(unirArray);
+}
