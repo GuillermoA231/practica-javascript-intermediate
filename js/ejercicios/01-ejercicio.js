607736d12de3c76edc118cb63a6d607a4a417bbe
GuillermoA231 <andradajoaquin302@gmail.com>
2023-03-12 21:39:16 -0300

    arregla typo faltaban los parentesis en la funcion

--- js/ejercicios/01-ejercicio.js
+++ js/ejercicios/01-ejercicio.js
@@ -1,5 +1,5 @@
 /* 1- Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje. */
-function ejercicio1{
+function ejercicio1(){
   let edad;
   do {
     edad = prompt("Ingrese su edad");
