094c4af0dcf739504603fc40c1ed8c526d01a651
GuillermoA231 <andradajoaquin302@gmail.com>
2023-03-12 22:12:47 -0300

    function ejercicio14() creado

--- js/ejercicios/14-ejercicio.js
+++ js/ejercicios/14-ejercicio.js
@@ -1,4 +1,5 @@
-/*14- Realiza un script que pida una cadena de texto
+function ejercicio14(){
+    /*14- Realiza un script que pida una cadena de texto
 y lo muestre poniendo el signo – entre cada carácter sin usar el método replace.
 Por ejemplo, si tecleo “hola qué tal”, deberá salir “h-o-l-a- -q-u-e- -t-a-l”.
  */
@@ -11,3 +12,4 @@ for (let i = 0; i < texto.length; i++) {
 }
 textoConcatenado = textoConcatenado.slice(0,textoConcatenado.length-1);
 console.log(textoConcatenado);
+}
